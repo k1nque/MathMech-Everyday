ff09a0bf972b513468c5da8951ce7078aea7dbc3
k1nque <artkashaev@gmail.com>
2021-10-28 05:53:13 +0500

    first commit

---
+++ MathMech Everyday/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathMech_Everyday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
