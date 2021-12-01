c25c15f632922c24fc7ba39bdf3a284ab602f7c4
k1nque <72624283+k1nque@users.noreply.github.com>
2021-12-01 18:39:50 +0500

    Daily FreeRooms update

--- MathMech Everyday/Program.cs
+++ MathMech Everyday/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.InteropServices;
+using NCron;
+using NCron.Fluent;
+using NCron.Fluent.Crontab;
+using NCron.Fluent.Generics;
+using NCron.Service;
+using Parser.TimerTasks;
+
 
 namespace MathMech_Everyday
 {
     class Program
     {
+        public void Main()
+        {
+
+            var shedulingService = new SchedulingService();
+            shedulingService.Daily().Run<TimerTasks>();
+        }
     }
 
 }
---
+++ Parser/ScheduleTasks/FreeAuditories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCron;
+
+namespace Parser.TimerTasks
+{
+    public class TimerTasks : NCron.CronJob
+    {
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
