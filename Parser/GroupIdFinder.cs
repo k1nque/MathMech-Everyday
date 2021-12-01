987bc1bdcd59f75da06a936e20caaba78c05f267
Cockamamie <bugrymenko@yandex.ru>
2021-12-02 04:40:01 +0500

    Added method that finds all group ids in specific institute

--- Parser/GroupIdFinder.cs
+++ Parser/GroupIdFinder.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Parser
 {
@@ -18,18 +19,40 @@ namespace Parser
             public string title { get; set; }
         }
         
-        private static string GetJson() =>
-            new WebClient().DownloadString("https://urfu.ru/api/schedule/groups/");
+        private static string GetJson(string instituteId = default) =>
+            new WebClient().DownloadString(
+                $"https://urfu.ru/api/schedule/groups/{instituteId}");
 
         private static List<ParseProperties> ParseJson(string json) =>
             JsonSerializer.Deserialize<List<ParseProperties>>(json);
 
-        public static int FindGroupId(string id)
+        public static string FindGroupId(string id)
         {
             var json = GetJson();
             var properties = ParseJson(json)
                 .Where(property => property.title == id.ToUpper());
-            return properties.First().id;
+            return properties.First().id.ToString();
+        }
+        
+        private static IEnumerable<ParseProperties> SelectMathMechGroups(List<ParseProperties> properties)
+        {
+            var regex = new Regex("МЕНМ?-..(?:020[1678]|220[13]|010[12]|0705|" +
+                                  "08[01][0129]|10[01][15]|3201|0[12789][01][0123])");
+            return properties
+                .Where(property => regex.IsMatch(property.title));
+        }
+        
+        public static IEnumerable<string> FindInstituteGroupsId(IEnumerable<string> institutesIds)
+        {
+            var ids = new List<IEnumerable<int>>();
+            foreach (var id in institutesIds)
+            {
+                var json = GetJson(id);
+                var parseProperties = ParseJson(json);
+                var mathMechProperties = SelectMathMechGroups(parseProperties);
+                ids.Add(mathMechProperties.Select(properties => properties.id));
+            }
+            return ids.SelectMany(list => list.Select(id => id.ToString()));
         }
     }
 }
