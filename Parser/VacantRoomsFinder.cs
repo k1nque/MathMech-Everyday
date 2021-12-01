78a8b87a34deb8e6230bf5d7b82ef2d72871ab6c
Cockamamie <bugrymenko@yandex.ru>
2021-12-01 18:10:09 +0500

    Created class that finds vacant cabinets

---
+++ Parser/VacantRoomsFinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parser
+{
+    public class VacantRoomsFinder
+    {
+        private static IEnumerable<DaySchedule> GetGroupsSchedule(
+            IEnumerable<string> groupsId, DateTime dateTime) =>
+            groupsId.Select(id => ScheduleCreator.CreateScheduleById(id, dateTime)
+                .GetDaySchedule(dateTime.DayOfWeek));
+
+        private static IEnumerable<Lesson> GetLessonsAtTime(
+            IEnumerable<DaySchedule> groupsDaySchedule, DateTime dateTime) =>
+            groupsDaySchedule
+                .SelectMany(schedule => schedule.Schedule
+                    .Where(lesson => lesson.Start <= dateTime && dateTime <= lesson.End));
+
+        private static IEnumerable<string> GetLessonsRooms(IEnumerable<Lesson> lessons) =>
+            lessons.Select(lesson => lesson.Location);
+
+        public IEnumerable<string> FindVacant(DateTime dateTime) //institute?
+        {
+            var ids = new List<string>();
+            var groupsSchedule = GetGroupsSchedule(ids, dateTime);
+            var lessons = GetLessonsAtTime(groupsSchedule, dateTime);
+            return GetLessonsRooms(lessons);
+        }
+    }
+}
