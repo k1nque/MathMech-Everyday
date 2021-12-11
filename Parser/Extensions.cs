6418e7a07bdc8945498c0cb7e6c5f6f134335df6
Cockamamie <bugrymenko@yandex.ru>
2021-12-11 16:54:06 +0500

    Created extension methods

---
+++ Parser/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ical.Net.DataTypes;
+
+namespace Parser
+{
+    public static class Extensions
+    {
+        public static DateTime GetLastSunday(this DateTime dateTime) =>
+            dateTime.AddDays(-(int) dateTime.DayOfWeek);
+        
+        public static DateTime ToDateTime(this IDateTime dt) =>
+            new(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+    }
+}
