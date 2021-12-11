b7355fe613391cbc55a97019da92da2561e120c1
Cockamamie <bugrymenko@yandex.ru>
2021-12-11 16:56:22 +0500

    Using list of groups instead of regex

--- Parser/GroupIdFinder.cs
+++ Parser/GroupIdFinder.cs
@@ -26,19 +26,27 @@ namespace Parser
             JsonSerializer.Deserialize<List<ParseProperties>>(json);
 
         public static string FindGroupId(string id)
+        private IEnumerable<GroupsDataParser> SelectMathMechGroups(IEnumerable<GroupsDataParser> properties)
         {
             var json = GetJson();
             var properties = ParseJson(json)
                 .Where(property => property.title == id.ToUpper());
             return properties.First().id.ToString();
+            var groups = GetGroups("MathMech.txt");
+            return properties.Where(p => groups.Contains(p.Title));
         }
         
         private static IEnumerable<ParseProperties> SelectMathMechGroups(List<ParseProperties> properties)
+
+        private HashSet<string> GetGroups(string fileName)
         {
             var regex = new Regex("МЕНМ?-..(?:020[1678]|220[13]|010[12]|0705|" +
                                   "08[01][0129]|10[01][15]|3201|0[12789][01][0123])");
             return properties
                 .Where(property => regex.IsMatch(property.title));
+            var folderPath = Path.GetFullPath(@"..\..\..\..\Parser\GroupsData\");
+            var data = File.ReadLines(folderPath + fileName);
+            return data.ToHashSet();
         }
         
         public static IEnumerable<string> FindInstituteGroupsId(IEnumerable<string> institutesIds)
