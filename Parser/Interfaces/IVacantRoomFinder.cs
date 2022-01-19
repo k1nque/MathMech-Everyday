aa883d53e49fbb82ff58f75c69c178b090ff12dd
Cockamamie <bugrymenko@yandex.ru>
2022-01-20 03:56:19 +0500

    Implement interface for DI container

---
+++ Parser/Interfaces/IVacantRoomFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public interface IVacantRoomFinder
+    {
+        Task<IEnumerable<string>> FindVacant(DateTime timeToFind);
+    }
+}
